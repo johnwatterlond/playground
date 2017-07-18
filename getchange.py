4bcf188de2cb642c89191b534fc8e81543ba42f5
john watterlond <john.watterlond@gmail.com>
2017-07-17 19:17:07 -0700

    Cleaning up code.

--- getchange.py
+++ getchange.py
@@ -5,8 +5,13 @@ Module to calculate change.
 import collections
 
 
-coin_tuples = [('quarters', 25), ('dimes', 10), ('nickels', 5), ('pennies', 1)]
-coin_vals = collections.OrderedDict(coin_tuples)
+coin_names = ['quarters', 'dimes', 'nickels', 'pennies']
+coin_values = [25, 10, 5, 1]
+coins = collections.OrderedDict(zip(coin_names, coin_values)))
+
+dollar_names = ['twenties', 'tens', 'fives', 'ones']
+dollar_values = [20, 10, 5, 1]
+dollars = collections.OrderedDict(zip(dollar_names, dollar_values))
 
 
 def get_coins(change):
@@ -23,11 +28,32 @@ def get_coins(change):
         'nickels':0,
         'pennies':0
         }
-    for coin in coin_vals:
-        (coin_count, new_change) = divmod(change, coin_vals[coin])
+    for coin in coins:
+        (coin_count, new_change) = divmod(change, coins[coin])
         coin_counts[coin] = coin_count
         change = new_change
     return coin_counts
+
+
+def get_dollars(change):
+    """
+    Return a dictionary of coin_name:coin_count needed to give
+    change for coins.
+
+    The variable change should be an integer.
+    To get coins for .35 dollars, call get_coins(35).
+    """
+    dollar_counts = {
+        'twenties':0,
+        'tens':0,
+        'fives':0,
+        'ones':0
+        }
+    for dollar in dollars:
+        (dollar_count, new_change) = divmod(change, dollars[dollar])
+        dollar_counts[coin] = dollar_count
+        change = new_change
+    return dollar_counts
 
 
 def find_change(cost, given):
@@ -39,27 +65,51 @@ def find_change(cost, given):
 
 
 def print_coin_change(coin_dict):
-    change_str = 'Quarters: {quarters}\nDimes: {dimes}\nNickels: {nickels}\nPennies: {pennies}'
+    """
+    Print out coin names and amounts.
+
+    Expects a dictionary from get_coins.
+    """
+    change_str = (
+        'Quarters: {quarters}\n'
+        'Dimes: {dimes}\n'
+        'Nickels: {nickels}\n'
+        'Pennies: {pennies}'
+        )
     print(change_str.format(**coin_dict))
+
+
+def print_dollar_change(dollar_dict):
+    """
+    Print out dollar names and amounts.
+
+    Expects a dictionary from get_dollars.
+    """
+    change_str = (
+        'Twenties: {twenties}\n'
+        'Tens: {tens}\n'
+        'Fives: {fives}\n'
+        'Ones: {ones}'
+        )
+    print(change_str.format(**dollar_dict))
 
 
 def calc_and_print_change(cost, given):
     dollars, cents = find_change(cost, given)
+
     coin_count_dict = get_coins(cents)
-    print('Dollars: {}'.format(dollars))
+    dollar_count_dict = get_dollars(dollars)
+
+    print('Your change is {}.{}'.format(dollars, cents))
+    print_dollar_change(dollar_count_dict)
     print_coin_change(coin_count_dict)
 
 
+def main():
+    cost = input('How much does customer owe? ')
+    given = input('How much did customer give? ')
+    calc_and_print_change(cost, given)
 
 
-
-
-#TODO:
-# output should be:
-# Your change is: blablalba.blablal
-# That is:
-# twenties: bla
-# tens: bla
-# fives: bla
-# ones: bla
-# print_coin_change...
+if __name__ == '__main__':
+    main()
